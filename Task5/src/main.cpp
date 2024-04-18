3851ac86c10b7909b45a25ff96bcf15371d2281e
ube14020905 <ubedullashaik11@gmail.com>
2024-04-18 14:41:24 +0530

    Create MakeFile

--- Task5/src/main.cpp
+++ Task5/src/main.cpp
@@ -2,10 +2,13 @@
 #include<iostream>
 
 using namespace std;
-int main(){
-	string jsonpath;
-	cout<<"Enter Source JSON File path: ";
-	cin>>jsonpath;
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        cerr << "Usage: " << argv[0] << " <JSON_file>" << endl;
+        return 1;
+    }
+	string jsonpath=argv[1];
+
 	string configpath="./config/configuration.json";
 	httpjson *server=new httpjson(jsonpath,configpath);
 	JsonApp config(configpath);
@@ -13,9 +16,9 @@ int main(){
 	map<string, SBU2WebService *> * jWebService = new map<string, SBU2WebService *>;
 	// string end_url = obj["httpServer"]["endUrl"].toString();
 	string end_url = "/content";
-	int load =1;// stoi(obj["httpServer"]["load"].toString());
+	int load = 1; // stoi(obj["httpServer"]["load"].toString());
 	int port = 8080;//stoi(obj["httpServer"]["port"].toString());
-	cout << "end url: " << end_url << "Load :" << load << "port: " << port << endl;
+	cout << "end url: " << end_url << "\tLoad :" << load << "\tport: " << port << endl;
 	(*jWebService)[end_url] = server;
 	SBU2LoadBalancer *loadBalancer = new SBU2LoadBalancer(load,jWebService);
 	SBU2HTTPServer *httpServer = new SBU2HTTPServer(port , loadBalancer);
